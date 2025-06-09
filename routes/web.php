2b35d4b9bd4fae1511d7e746b240d88523399bec
DeadPage7 <lolol.kek154@gmail.com>
2025-06-09 09:55:08 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\MaterialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/material', [MaterialController::class,'index'])->name('index');
+Route::get('/material/create', [MaterialController::class,'create'])->name('create');
+Route::post('/material', [MaterialController::class,'store'])->name('store');
+Route::get('/material/{material}/edit', [MaterialController::class,'edit'])->name('edit');
+Route::get('/material/{material}', [MaterialController::class,'show'])->name('show');
